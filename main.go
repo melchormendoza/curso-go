6fc223886fec63b82e663b6c2eab6773b3bb976a
Melchor Mendoza <melchormendoza@MacBook.local>
2022-01-13 08:59:02 -0600

    variables con asignacion dinamica

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	//Declaracion de variables
+	var numero int
+	var nombre, apellidos string
+	//asignacion de valores a variables
+	numero = 10
+	nombre = "Melchor"
+	apellidos = "Mendoza Ramirez"
+	ciudad := "Oaxaca"
+	fmt.Println("Hello world!", numero, nombre, apellidos, ciudad)
 }
